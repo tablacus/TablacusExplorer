f58f60510681b51d62abbac383b5c934abdab51d
Gaku <rxf10242@gmail.com>
2020-02-29 23:33:33 +0900

    20.2.29 Some fix

--- Debug/script/common.js
+++ Debug/script/common.js
@@ -2,7 +2,7 @@
 
 function AboutTE(n) {
 	if (n == 0) {
-		return te.Version < 20200222 ? te.Version : 20200225
+		return te.Version < 20200229 ? te.Version : 20200229
 	}
 	if (n == 1) {
 		var v = AboutTE(0);
