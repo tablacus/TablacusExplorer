da55e29b4e950d3ae2d169d660870a862c149c50
Gaku <rxf10242@gmail.com>
2020-08-28 06:42:23 +0900

    20.8.27 Adjusted: Tree view

--- Debug/script/common.js
+++ Debug/script/common.js
@@ -2,7 +2,7 @@
 
 function AboutTE(n) {
 	if (n == 0) {
-		return te.Version < 20200825 ? te.Version : 20200825;
+		return te.Version < 20200827 ? te.Version : 20200827;
 	}
 	if (n == 1) {
 		var v = AboutTE(0);
