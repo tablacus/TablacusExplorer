497a96700cb396ae38534337716afa08e40073f3
Gaku <rxf10242@gmail.com>
2020-10-22 22:33:26 +0900

    20.10.22 Adjusted: Mouse gesture, Blink project

--- Debug/script/sync.js
+++ Debug/script/sync.js
@@ -77,7 +77,7 @@ if (g_.IEVer < 10) {
 
 AboutTE = function (n) {
 	if (n == 0) {
-		return te.Version < 20201019 ? te.Version : 20201021;
+		return te.Version < 20201019 ? te.Version : 20201022;
 	}
 	if (n == 1) {
 		var v = AboutTE(0);
