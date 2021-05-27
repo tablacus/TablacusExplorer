4a26e3550c0aaa6ab0b9a91df045a70b04b0853d
Gaku <rxf10242@gmail.com>
2021-05-27 21:44:56 +0900

    21.5.27

--- Debug/addons/download/script.js
+++ Debug/addons/download/script.js
@@ -3,7 +3,12 @@ if (window.Addon == 1) {
 	importJScript("addons\\" + Addon_Id + "\\sync.js");
 } else {
 	SetTabContents(0, "", await ReadTextFile("addons\\" + Addon_Id + "\\options.html"));
-	document.getElementById("_curl").value = await api.sprintf(99, await GetText("Get %s..."), "cURL");
+	const el = document.getElementById("_curl");
+	if (await fso.FileExists(BuildPath(system32, "curl.exe"))) {
+		el.style.display = "none";
+	} else {
+		el.value = await api.sprintf(99, await GetText("Get %s..."), "cURL");
+	}
 	document.getElementById("_wget").value = await api.sprintf(99, await GetText("Get %s..."), "Wget");
 
 	SetDL = async function (o) {
