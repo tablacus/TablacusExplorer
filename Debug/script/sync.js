9f089fb933929d1a46182b950474b3f4bd28fbf0
Gaku <rxf10242@gmail.com>
2021-12-15 23:06:32 +0900

    Update sync.js

--- Debug/script/sync.js
+++ Debug/script/sync.js
@@ -57,7 +57,7 @@ g_.updateJSONURL = "https://api.github.com/repos/tablacus/TablacusExplorer/relea
 
 AboutTE = function (n) {
 	if (n == 0) {
-		return te.Version < 20211210 ? te.Version : 20211214;
+		return te.Version < 20211210 ? te.Version : 20211215;
 	}
 	if (n == 1) {
 		const v = AboutTE(0);
