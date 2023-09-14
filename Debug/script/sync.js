0649e07420c789d2c288a65f905ddff1586260b0
Gaku <rxf10242@gmail.com>
2023-09-14 22:07:10 +0900

    23.9.13 Some fix

--- Debug/script/sync.js
+++ Debug/script/sync.js
@@ -66,7 +66,7 @@ g_.IconChg = [
 
 AboutTE = function (n) {
 	if (n == 0) {
-		return te.Version < 20230710 ? te.Version : 20230819;
+		return te.Version < 20230913 ? te.Version : 20230913;
 	}
 	if (n == 1) {
 		const v = AboutTE(0);
