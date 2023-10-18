c14f2c0764c8a50b706f858a3db15984bbf40391
Gaku <rxf10242@gmail.com>
2023-10-18 22:29:10 +0900

    23.10.18

--- Debug/addons/treeview/sync.js
+++ Debug/addons/treeview/sync.js
@@ -161,9 +161,11 @@ if (WINVER >= 0x600) {
 			const hLock = api.SHChangeNotification_Lock(wParam, lParam, pidls);
 			if (hLock) {
 				api.SHChangeNotification_Unlock(hLock);
-				const cFV = te.Ctrls(CTRL_FV);
-				for (let i in cFV) {
-					cFV[i].TreeView.Notify(pidls.lEvent, pidls[0], pidls[1], wParam, lParam);
+				if (pidls[0] && /^[A-Z]:\\|^\\\\\w/i.test(pidls[0].Path) && !IsCloud(pidls[0])) {
+					const cFV = te.Ctrls(CTRL_FV);
+					for (let i in cFV) {
+						cFV[i].TreeView.Notify(pidls.lEvent, pidls[0], pidls[1], wParam, lParam);
+					}
 				}
 			}
 			return S_OK;
