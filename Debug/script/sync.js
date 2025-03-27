4cfe8c767b81cd8cbf0e8cf75b2f634b3894360b
Gaku <rxf10242@gmail.com>
2025-03-27 21:55:39 +0900

    25.3.27 Some fix

--- Debug/script/sync.js
+++ Debug/script/sync.js
@@ -67,7 +67,7 @@ g_.Notify = {};
 
 AboutTE = function (n) {
 	if (n == 0) {
-		return te.Version < 20250227 ? te.Version : 20250311;
+		return te.Version < 20250224 ? te.Version : 20250327;
 	}
 	if (n == 1) {
 		const v = AboutTE(0);
