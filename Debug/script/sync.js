cd343dbc7da0617e8c4c73cc19274771bd6055f6
Gaku <rxf10242@gmail.com>
2025-04-01 22:36:17 +0900

    25.4.1 Thank you for everything, Trident

--- Debug/script/sync.js
+++ Debug/script/sync.js
@@ -67,7 +67,7 @@ g_.Notify = {};
 
 AboutTE = function (n) {
 	if (n == 0) {
-		return te.Version < 20250224 ? te.Version : 20250327;
+		return te.Version < 20250224 ? te.Version : 20250401;
 	}
 	if (n == 1) {
 		const v = AboutTE(0);
